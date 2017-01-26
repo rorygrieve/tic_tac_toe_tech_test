39725db5851c4c2822e7bc9955c3aac49f000b53
pflarcology <rhrg500@gmail.com>
2017-01-26 11:15:12 +0000

    game initializes with a player 1 that equals nil

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,10 @@
 class Game
 
+  attr_reader :player1
+  
+  def initialize
+    @player1 = nil
+  end
+
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,4 +2,11 @@ require 'game'
 
 describe Game do
 
+  let!(:game) { described_class.new }
+
+  it "should initialize with a player 1 that equals nil" do
+    expect(game.player1).to eq(nil)
+  end
+
+
 end
