7e734aa4f1ddfa53453a7f3fb7b511ceb14c7c95
pflarcology <rhrg500@gmail.com>
2017-01-26 11:07:59 +0000

    player initializes with a name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,4 +2,10 @@ require 'player'
 
 describe Player do
 
+  let!(:player) { described_class.new("rory") }
+
+  it "should intialize with a name" do
+    expect(player.name).to eq("rory")
+  end
+
 end
