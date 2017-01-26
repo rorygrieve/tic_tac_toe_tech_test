4579bec1f7df1ad4c6547c859616b21653b4db39
pflarcology <rhrg500@gmail.com>
2017-01-26 14:30:40 +0000

    adds test back in on changing board

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -55,6 +55,10 @@ describe Game do
       expect(game.turn).to eq(player1double)
     end
 
+    it "place mark should change the board" do
+      game.place_mark(2,2)
+      expect(game.board.show).to eq([['','',''], ['','X',''], ['','','',]])
+    end
 
   end
 
